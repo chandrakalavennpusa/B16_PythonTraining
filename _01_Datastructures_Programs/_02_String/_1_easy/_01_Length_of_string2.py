46a03fd458f37fe2ecf841d4cd28c3135c3d7692
venkateshgurram337 <chowdaryvenkatesh419@gmail.com>
2021-05-11 10:03:47 +0545

    commiting files

---
+++ _01_Datastructures_Programs/_02_String/_1_easy/_01_Length_of_string2.py
@@ -0,0 +1,49 @@
+# P01. REQ : Find length of given string
+'''
+Operators
+DM,Loops
+Data structure
+'''
+
+# 1.Builtin functions
+print("-----1.Built Functions--------")
+
+message = 'Hello World'
+print("Length of given string : ", len(message))
+
+# 2. Algorithm ***
+print("--------2. Algorithm----------")
+
+message = 'Hello World'
+le = 0
+for char in message:
+    le += 1
+print("Length of given string : ", le)
+
+# 3 Using Functions  ==> 10
+print("--------3 Using Functions----------")
+    # I. STATE
+message = 'Hello World'
+le = 0
+    # BEHAVIOR
+def get_str_len(msg):
+    leng = 0
+    for char in msg:
+        leng += 1
+    print("Length of given string : ", leng)
+
+get_str_len(message)
+
+
+
+
+# 4 OOPS  ==> 5
+print("--------4 Object Oriented----------")
+# 5 Exception handling  ==> 2
+print("--------5 Exception handling----------")
+# 6 File Handling  ==> 1
+print("--------6 File Handling----------")
+# 6 Database interaction MVC  ==> 1
+print("--------6 Database interaction----------")
+# 7 UI Interaction   ==> 1
+print("--------7 UI Interaction----------")
