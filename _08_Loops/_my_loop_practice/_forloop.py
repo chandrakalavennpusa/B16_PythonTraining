1de8ca22e32294b2831fe2315067face9a903f82
chandrakalavennapusa <chandrakalavennapusa877@gmail.com>
2021-05-15 18:20:42 +0530

    while loop on first 20 even numbers from 100 to 150

---
+++ _08_Loops/_my_loop_practice/_forloop.py
@@ -0,0 +1,24 @@
+num = 1
+counter = 0
+while num <= 100:
+    if counter > 7:
+        break
+    if num % 2 == 0:
+        print(num)
+        counter += 1
+    num += 1
+print("Exited while loop")
+
+
+
+###print  20 numbers 1000 to 1500 which are even numbers ###
+num = 1000
+counter = 0
+while num <= 1500:
+    if counter < 20:
+        break
+    if num % 2 == 0:
+        print("num")
+        count += 1
+    num += 1
+print("come out of loop")
