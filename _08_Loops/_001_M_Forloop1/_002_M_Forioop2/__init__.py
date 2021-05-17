27e8d0cda6067c5720eae6da66ae09506ff1a30d
chandrakalavennapusa <chandrakalavennapusa877@gmail.com>
2021-05-17 19:15:21 +0530

    for loop M.Notes

---
+++ _08_Loops/_001_M_Forloop1/_002_M_Forioop2/__init__.py
@@ -0,0 +1,37 @@
+
+
+######print of numbers using range##########
+
+for val in range(100, 200, 2):
+    print("Even number value : ", val)
+
+
+ #######print of odd numbers 1000 to 2000#######3 using range
+
+for val in range(1001, 2001, 2):
+    print("Odd number value : ", val)
+
+
+#####33print of multiples of 5 from 5 to 100 by using range#####
+
+
+for val in range(5,100,5):
+    print("5 multiple value : ", val)
+
+
+#### print of values from big number to small number####
+
+for val in range(100,5,-5):
+    print("5 multilple value : ", val)
+
+
+####3print of values from 0 to 100####
+for val in range(0,2000,50 ):
+    print("range value : ", val)
+
+
+for val in range(10, 20, 2):
+    print("range val : ", val)
+
+
+
---
+++ _08_Loops/_001_M_Forloop1/__init__.py
@@ -0,0 +1,79 @@
+
+#  print of employ_ids from list.......
+
+
+emp_ids = [25, 35, 78, 90, 101, 210, 320, 890]
+for eid in emp_ids:
+    print("Employee id :",eid)
+
+
+###### print of characters from tne string
+msg = 'welcome  world'
+for char in msg:
+    print("Character : ", char)
+
+
+msg = 'employment'
+for char in msg:
+    print("character : ", char)
+
+
+msg = 'holiday'
+for char in msg:
+    print("character :  ",  char)
+    print("msg")
+
+
+student_ids = [1, 2, 3, 4, 5, 6, 7, 8.]
+for s_id in student_ids:
+    print("Student_id : ", s_id)
+
+    ### use of dictionery....
+emp_record = {"emp_id": 201,
+              "emp_name": "chandra kala",
+              "emp_sal": 25000}
+for key, value in emp_record.items():
+    print(key, value)
+
+
+
+#### print of numbers in range....
+
+
+for value in range(20):
+    print("Rangevalue : ", value)
+
+
+for value in range(1,21):
+    print("Range value : ", value)
+
+
+for value in range(-30, -10, 2):
+    print("Range value : ", value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
